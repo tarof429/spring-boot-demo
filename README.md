700aebebb30346b02780a04a3ec71d8f82428417
Taro Fukunaga <tarof429@gmail.com>
2025-01-23 21:10:51 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Spring Boot Demo
+
+Example Spring Boot application based on https://spring.io/guides/gs/spring-boot. 
---
+++ src/main/java/com/taro/demo/HelloController.java
@@ -0,0 +1,19 @@
+package com.taro.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+
+	@GetMapping("/test")
+	public String getTest() {
+		return "Test worked!";
+	}
+
+}
